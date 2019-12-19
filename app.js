45cd437356c217235f443ad62b0747c12044e002
Enno Boland <g@s01.de>
2019-12-19 15:02:41 +0100

    initial checkin

---
+++ app.js
@@ -0,0 +1,31 @@
+const IRC = require('irc-framework')
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const client = new IRC.Client();
+client.connect({
+	host: core.getInput('server'),
+	port: core.getInput('port'),
+	// TODO password <- server password
+	nick: core.getInput('nickname'),
+	password: core.getInput('sasl_password'),
+	tls: core.getInput('tls'),
+});
+const options = {
+	message: core.getInput('message'),
+	notice: core.gettInput('notice'),
+	channel: core.getInput('channel'),
+	channel_key: core.getInput('channel_key')
+}
+
+client.on('registered', function() {
+	let channel = client.channel(options.channel, options.channel_key);
+	if (options.notice) {
+		channel.notice(options.message);
+	} else {
+		channel.join();
+		channel.say(options.message);
+	}
+	client.quit("Bye!");
+});
